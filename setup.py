aa9cae8504a8b88a2319e82941bc40af3cb44982
mrjaber <jaberaldhlme@gmail.com>
2026-05-16 00:15:27 +0300

    requirments and installation was susscessful

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+
+__version__ = "0.0.0"
+REPO_NAME="textsummrizor"
+AUTHOR_USER_NAME="jaberaldhlme-maker"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL="jaberaldhlme@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A simple text summarization library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
